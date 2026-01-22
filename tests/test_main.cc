e2e91b27b314e90176d71d79b15f078c81e20441
zarond <55189420+zarond@users.noreply.github.com>
2026-01-22 20:11:54 +0100

    Add tests and github action

--- tests/test_main.cc
+++ tests/test_main.cc
@@ -1,5 +1,16 @@
 #include <gtest/gtest.h>
 
+#include <cstdlib>
+#include <filesystem>
+
 TEST(SampleTest, Addition) {
     EXPECT_EQ(2 + 2, 4);
+}
+
+TEST(SampleTest, Rendering) {
+    const char* command = "PathTracer -no-gui -f ./scene.glb -e ./farmland_overcast_1k.hdr -o snapshot.png -s 20 -b 6";
+    int result = std::system(command);
+    EXPECT_EQ(result, 0);
+    bool file_exists = std::filesystem::exists("snapshot.png") && std::filesystem::is_regular_file("snapshot.png");
+    EXPECT_EQ(file_exists, true);
 }
